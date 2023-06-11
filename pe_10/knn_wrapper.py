8c78f21333ee1934303839e9040d1e98da02888f
LanNetwork <ianmcgeoy@gmail.com>
2023-06-11 13:20:11 -0700

    Completed pe_10

--- pe_10/knn_wrapper.py
+++ pe_10/knn_wrapper.py
@@ -1,35 +1,65 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import datasets
+from sklearn.metrics import accuracy_score
 import pandas as pd
 import csv
 
 
 class knnWrapperClass:
     def __init__(self):
-        knn = None
-        df_train = None
-        df_train_target = None
-        df_test = None
-        df_test_target = None
+        self.knn = None
+        self.df_train = None
+        self.df_train_target = None
+        self.df_test = None
+        self.df_test_target = None
+        self.predicted_values = None
 
     def train(self, neighbors):
-        """...write comment about this method...
+        """Train the k-nearest neighbors classifier.
+
+        Args:
+            neighbors (int): The number of neighbors to consider.
+
         """
-        # TODO: function logic
+        self.knn = KNeighborsClassifier(n_neighbors=neighbors)
+        self.knn.fit(self.df_train, self.df_train_target)
 
     def predict(self):
-        """...write comment about this method...
-        """
-        # TODO: function logic
+        """Predict the classes of the test data.
 
-        return 0
+        Returns:
+            numpy.ndarray: The predicted classes.
+
+        """
+        self.predicted_values = self.knn.predict(self.df_test)
+        return self.predicted_values
 
     def import_train_file(self, trainDataFile, trainTargetFile):
-        """...write comment about this method...
+        """Import the training data and target CSV file.
+
+        Args:
+            trainDataFile (str): The filename of the training data CSV file.
+            trainTargetFile (str): The filename of the training target CSV file.
+
         """
-        # TODO: function logic
+        self.df_train = pd.read_csv(trainDataFile)
+        self.df_train_target = pd.read_csv(trainTargetFile)
 
     def import_test_file(self, testDataFile, testTargetFile):
-        """...write comment about this method...
+        """Import the test data and target CSV file.
+
+        Args:
+            testDataFile (str): The filename of the test data CSV file.
+            testTargetFile (str): The filename of the test target CSV file.
+
         """
-        # TODO: function logic
+        self.df_test = pd.read_csv(testDataFile)
+        self.df_test_target = pd.read_csv(testTargetFile)
+
+    def accuracy(self):
+        """Return accuracy difference between predicted and actual labels.
+
+        Returns:
+            int: accuracy rating        
+        """
+        return accuracy_score(self.predicted_values, self.df_test_target)
--- pe_10/knn_wrapper_main.py
+++ pe_10/knn_wrapper_main.py
@@ -17,25 +17,56 @@ In short paragraph, provide which 'neighbor' parameter works best for each datas
 
 
 """
+
+""" IAN MCGEOY STUDENT ANSWER:
+    n value of 1 worked best for both datasets. I would reccomend automating the selection of an n value,
+    because now that I have a function to do that, you can use it any time you're using a knn model.
+    """
 from knn_wrapper import knnWrapperClass
 
 
 def main():
-
+    
+    best_n, best_acc = find_best_n(("iris_data.csv", "iris_target.csv"), ("iris_test_data.csv", "iris_test_target.csv"))
+    # print(f'Best n: {best_n}\nAcc: {best_acc}')
     knnW = knnWrapperClass()
     knnW.import_train_file("iris_data.csv", "iris_target.csv")
     # have set neighbor n value as 2, but please find better value
-    knnW.train(2)
+    print(f'Using n = {best_n}')
+    knnW.train(best_n)
     knnW.import_test_file("iris_test_data.csv", "iris_test_target.csv")
     result = knnW.predict()
     print(result)
 
+    best_n, best_acc = find_best_n(("wine_data.csv", "wine_target.csv"), ("wine_test_data.csv", "wine_test_target.csv"))
+    # print(f'Best n: {best_n}\nAcc: {best_acc}')
     knnW = knnWrapperClass()
     knnW.import_train_file("wine_data.csv", "wine_target.csv")
-    knnW.train(2)
+    print(f'Using n = {best_n}')
+    knnW.train(best_n)
     knnW.import_test_file("wine_test_data.csv", "wine_test_target.csv")
     result = knnW.predict()
     print(result)
+
+
+def find_best_n(training_data: tuple, testing_data: tuple, starting_n: int = 1, limit: int = 15):
+    # This function tests the knn wrapper on it's imported datasets with different values
+    # of n, until either accuracy == 1, or limit is met.
+    best_n = None
+    best_accuracy = 0
+    for n in range(starting_n, limit):
+        knn = knnWrapperClass()
+        knn.import_train_file(training_data[0], training_data[1])
+        knn.import_test_file(testing_data[0], testing_data[1])
+        knn.train(n)
+        knn.predict()
+        acc = knn.accuracy()
+        if acc > best_accuracy:
+            best_n = n
+            best_accuracy = acc
+            if best_accuracy == 1:
+                break
+    return (best_n, best_accuracy)
 
 
 if __name__ == "__main__":
